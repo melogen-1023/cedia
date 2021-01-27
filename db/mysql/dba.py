2f5d9a780faa5e4086d23033336e8c5661957792
melogen-1023 <2379415638@qq.com>
2021-01-27 21:21:00 +0800

    update bilibili.py

--- db/mysql/dba.py
+++ db/mysql/dba.py
@@ -13,25 +13,21 @@ class CediaDBA:
 
     def insert(self, data):
         cursor = self.mysql_cnx.cursor()
-        data = {
-            'id': media_id_generator.next_id(),
-            'title': 'test',
-            'name': 'test',
-            'content': 'test',
-            'kind': 0,
-            'date': datetime.date.today(),
-            'duration': 112,
-            'coverImageType': 'image/jpg'
-        }
 
         add_media = ("INSERT INTO filmtube_video"
                      "(id,name,content,title,kind,date,duration,coverImageType)"
                      "VALUES(%s,%s,%s,%s,%s,%s,%s,%s)"
                      )
 
+        add_media_link = ("INSERT INTO filmtube_video_link"
+                          "(videoId, episode, type) "
+                          "VALUES (%s,%s,%s)"
+                          )
+
         cursor.execute(add_media, (
             data['id'], data['name'], data['content'], data['title'], data['kind'], data['date'], data['duration'],
             data['coverImageType']))
+        cursor.execute(add_media_link, (data['id'], 1, "video/mp4"))
         self.mysql_cnx.commit()
         cursor.close()
 
